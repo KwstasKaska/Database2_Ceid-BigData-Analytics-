65345fd40705384a948879356aacbed36fb81a73
Konstantinos Kaskantiris <58773683+PhobosNDeimos@users.noreply.github.com>
2020-11-18 12:21:08 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,4 @@
+USER02.YELPBUSINESS: CREATE TABLE ‘ USER02.YELPBUSINESS ’, ‘ BASE ’,
+‘ATTRIBUTES’, ‘ HOURS ’                                              /*3 column families:BASE,ATTRIBUTES,HOURS*/
+
+USER02.YELPCHECKIN: CREATE TABLE ‘ USER02.YELPCHECKIN ’, ‘ PERHOUR ’ /*1 column family:PERHOUR*/
